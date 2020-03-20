d161243e185430979c25fbaaa2a5313dfec20edf
zuzannaLawniczak <49079288+zuzannaLawniczak@users.noreply.github.com>
2020-03-20 18:12:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,42 +3,155 @@
 #include <stdlib.h>
 #include <time.h>
 
-unsigned int ns[] = { 10, /* TODO: fill in "n" i.e. instance sizes */ };
+using namespace std;
 
-void fill_increasing(int *t, unsigned int n) {
+unsigned int ns[] = { 10, 100, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+
+void fill_increasing(int *t, unsigned int n)
+{
     for (unsigned int i = 0; i < n; i++)
     {
         t[i] = i;
     }
 }
 
-void fill_decreasing(int *t, unsigned int n) {
+
+void fill_decreasing(int *t, unsigned int n)
+{
     for (unsigned int i = 0; i < n; i++)
     {
         t[i] = n - i - 1;
     }
 }
 
-void fill_vshape(int *t, unsigned int n) {
-    // TODO: implement
+
+void fill_vshape(int *t, unsigned int n)
+{
+    int v;
+    v = n / 2;
+    for(int i = 0; i < v; i++)
+    {
+        t[i] = (v - i) * 2;
+    }
+    for(int i = v; i < n; i++)
+    {
+        t[i] = (i - v) * 2 + 1;
+    }
 }
 
-void selection_sort(int *t, unsigned int n) {
-    // TODO: implement
+
+// SELECTION SORT
+void swap(int *t, int i, int j)
+{
+    int tmp = t[i];
+    t[i] = t[j];
+    t[j] = tmp;
 }
 
+int argmin(int *t, int s, int n)
+{
+    int argmin = s;
+    for(int i = s; i < n; i++)
+    {
+        if(t[i] < t[argmin]) argmin = i;
+    }
+    return argmin;
+}
+
+void selection_sort(int *t, unsigned int n)
+{
+    int i, j;
+    for(i = 0; i < n; i++)
+    {
+        j = argmin(t, i, n);
+        swap(t, i, j);
+    }
+}
+
+// INSERTION SORT
 void insertion_sort(int *t, unsigned int n) {
-    // TODO: implement
+    int key, i;
+    for(int j = 1; j < n; j++)
+    {
+        key = t[j];
+        i = j;
+        while(i > 0 && t[i-1] > key)
+        {
+            t[i] = t[i-1];
+            i--;
+        }
+        t[i] = key;
+    }
 }
 
-void quick_sort(int *t, unsigned int n) {
-    // TODO: implement
+// QUICK SORT 
+int partition(int *t, int p, int r)
+{
+    int x, i;
+    x = t[r];
+    i = p - 1;
+    for(int j = p; j <= r - 1; j++)
+    {
+        if(t[j] < x)
+        {
+            i = i + 1;
+            swap(t, i, j);
+        }
+    }
+    i = i + 1;
+    swap(t, i, r);
+    return i;
 }
+
+int random_partition(int *t, int p, int r)
+{
+    int i = p + (rand() % (r - p));
+    swap(t, i, r);
+    return partition(t, p, r);
+}
+
+void quick_sort(int *t, int p, int r)
+{
+    int q;
+    if(p < r)
+    {
+        q = partition(t, p, r); //dzielimy tablice na dwie czesci; q oznacza punkt podzialu
+        quick_sort(t, p, q - 1); //wywolujemy rekurencyjnie quicksort dla pierwszej czesci tablicy
+        quick_sort(t, q + 1, r); //wywolujemy rekurencyjnie quicksort dla drugiej czesci tablicy
+    }
+}
+
+void quick_sort(int *t, unsigned int n)
+{
+    quick_sort(t, 0, n - 1);
+}
+// HEAP SORT 
+
+void heapify(int *t, int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if(l < n && t[l] > t[largest]) largest = l;
+    if(r < n && t[r] > t[largest]) largest = r;
+    if(largest != i){
+        swap(t, i, largest);
+        heapify(t, n, largest); //rekurencyjnie
+    }
+}
+
 
 void heap_sort(int *t, unsigned int n) {
-    // TODO
+    for(int i = n / 2 - 1; i >= 0; i--){ //budowanie stosu, zmiana układu tablicy
+        heapify(t, n, i);
+    }
+    for(int i = n - 1; i >= 0; i--){ //wypakowywanie elemntów ze stosu jeden po drugim
+        swap(t, 0, i); 
+        heapify(t, i, 0); //wywołuje max
+    }
 }
 
+// LICZBY LOSOWE
 void fill_random(int *t, unsigned int n) {
     for (unsigned int i = 0; i < n; i++) {
         t[i] = rand();
@@ -84,9 +197,12 @@ void (*check_functions[])(int *, unsigned int) = { is_random, is_increasing, is_
 void (*sort_functions[])(int *, unsigned int) = { selection_sort, insertion_sort, quick_sort, heap_sort };
 
 char *fill_names[] = { "Random", "Increasing", "Decreasing", "V-Shape" };
-char *sort_names[] = { "SelectionSort", "InsertionSort", "QuickSort", "HeapSort" };
+char *sort_names[] = { "Selection sort", "Insertion sort", "Quick sort", "Heap sort" };
 
-int main() {
+int main()
+{
+    FILE * data;
+    data = fopen("data2.txt", "w");
     for (unsigned int i = 0; i < sizeof(sort_functions) / sizeof(*sort_functions); i++) {
         void (*sort)(int *, unsigned int) = sort_functions[i];
 
@@ -96,7 +212,7 @@ int main() {
 
             for (unsigned int k = 0; k < sizeof(ns) / sizeof(*ns); k++) {
                 unsigned int n = ns[k];
-                int *t = malloc(n * sizeof(*t));
+                int *t = (int *) malloc(n * sizeof(*t));
 
                 fill(t, n);
                 check(t, n);
@@ -106,10 +222,11 @@ int main() {
                 clock_t end = clock();
                 is_sorted(t, n);
 
-                printf("%s\t%s\t%u\t%f\n", sort_names[i], fill_names[j], n, (double)(end - begin) / (double) CLOCKS_PER_SEC);
+                fprintf(data, "%s, %s\t%u\t%f\n", sort_names[i], fill_names[j], n, (double)(end - begin) / (double) CLOCKS_PER_SEC);
                 free(t);
             }
         }
     }
+    fclose(data);
     return 0;
 }
